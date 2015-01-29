8dfbbc464a4c3e18b7f95569e4f8969cce6aec05
Anthony <awmorp@gmail.com>
2015-01-29 19:30:37 +1100

    JSON polyfill for old IE

--- polyfills.js
+++ polyfills.js
@@ -1,7 +1,7 @@
 /* Polyfills to make up for missing functionality in Internet Explorer */
 
 if( !Array.prototype.filter ) {
-  console.log( "Installing Array.filter polyfill" );
+/*  console.log( "Installing Array.filter polyfill" ); */
   Array.prototype.filter = function( fn, context ) {
     var i, l = this.length, result = [];
     for( i = 0; i < l; i++ ) {
@@ -17,7 +17,7 @@ if( !Array.prototype.filter ) {
 
 
 if( !Array.prototype.indexOf ) {
-  console.log( "Installing Array.indexOf polyfill" );
+/*  console.log( "Installing Array.indexOf polyfill" ); */
   Array.prototype.indexOf = function( obj, start ) {
     for( var i = (start ? start : 0), j = this.length; i < j; i += 1 ) {
       if( this[i] === obj ) {
